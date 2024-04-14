cd42678b9a33013176b8fea8bfc3608085a0ae22
Torreip <80983183+Torreip@users.noreply.github.com>
2024-04-14 14:05:42 +0200

    ; was added at the end of a line where it was unnessary

--- src/Components/Product/Products.jsx
+++ src/Components/Product/Products.jsx
@@ -45,7 +45,6 @@ function Products() {
                                 />
                             </Col>
                         ))}
-                        ;
                     </Row>
                 </>
             )}
